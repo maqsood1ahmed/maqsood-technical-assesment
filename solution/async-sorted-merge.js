521df65e2efc969eccbc3e167713bfcb826172b1
Nedians <maqsoodcs@gmail.com>
2022-07-20 14:31:09 +0500

    fix: shift sync code to async and make function sync using async/await

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -4,6 +4,28 @@
 
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
+    let entries = [];  // combine all sources entries
+
+    for(let i = 0; i < logSources.length; i++) { // loop through each source
+      const s = logSources[i];
+      while (!s.drained) { // run untill source drained
+        const entry = s.pop();
+        if (!entry) break;
+  
+        entries.push(entry);
+        
+        entries.sort((a, b) => {  // must sort all entries
+          var c = new Date(a.date);
+          var d = new Date(b.date);
+          return c - d;
+        });
+  
+        printer.print(entry);
+      }
+      printer.last = new Date(0);
+    };
+    
+    printer.done();
     resolve(console.log("Async sort complete."));
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,26 +2,32 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
-module.exports = (logSources, printer) => {
+module.exports = async (logSources, printer) => {
   let entries = [];  // combine all sources entries
-  logSources.forEach((s, index) => {  // loop through each source
-    while (!s.drained) { // run untill source drained
-      const entry = s.pop();
-      if (!entry) break;
 
-      entries.push(entry);
-      
-      entries.sort((a, b) => {  // must sort all entries
-        var c = new Date(a.date);
-        var d = new Date(b.date);
-        return c - d;
-      });
-
-      printer.print(entry);
-    }
+  for(let i = 0; i < logSources.length; i++) { // loop through each source
+    const s = logSources[i];
+    await new Promise((resolve, reject) => {
+      while (!s.drained) { // run untill source drained
+        const entry = s.pop();
+        if (!entry) {
+          resolve(null);
+          break;
+        }
+  
+        entries.push(entry);
+        
+        entries.sort((a, b) => {  // must sort all entries
+          var c = new Date(a.date);
+          var d = new Date(b.date);
+          return c - d;
+        });
+  
+        printer.print(entry);
+      }
+    });
     printer.last = new Date(0);
-    console.log('Now drained is true => ', s.drained, index);
-  });
+  };
   printer.done();
   return console.log("Sync sort complete.");
 };
